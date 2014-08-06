59ee6e06d3f6027d4f73c7f69577ea24d33b6621
Jerry Lundström <lundstrom.jerry@gmail.com>
2014-08-06 12:56:37 +0200

    Change autogen.sh to not reinstall INSTALL each time but reinstall other files as it did with --force.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
 echo "Running autoreconf" &&
-autoreconf --install --force
+rm -f config.guess \
+	config.sub \
+	install-sh \
+	ltmain.sh \
+	m4/libtool.m4 \
+	m4/ltoptions.m4 \
+	m4/ltsugar.m4 \
+	m4/ltversion.m4 \
+	m4/lt~obsolete.m4 &&
+autoreconf --install
