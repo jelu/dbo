0032605292bd25673320a1223022a720a57df3d6
Jerry Lundström <lundstrom.jerry@gmail.com>
2014-09-05 15:49:10 +0200

    Documenting example program

--- example/src/contacts.c
+++ example/src/contacts.c
@@ -24,6 +24,10 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
+/*
+ * This is an example program that utilies libdbo for storing person information
+ * that can be arranged into folders, like a contact book.
+ */
 
 #include "person.h"
 #include "folder.h"
@@ -33,6 +37,9 @@
 #include <string.h>
 #include <unistd.h>
 
+/*
+ * Display the commands that can be used.
+ */
 void usage(void) {
     fprintf(stderr, "Usage: example [command and arguments...]\n"
         "\n"
@@ -54,11 +61,15 @@ void usage(void) {
         );
 }
 
+/*
+ * This function is used to create a person object in the database.
+ */
 int contacts_person_create(libdbo_connection_t* connection, const char* name,
     const char* birth_year, const char* address, const char* phone,
     const char* email)
 {
     person_t* person;
+    int error;
 
     if (!connection) {
         return -1;
@@ -79,36 +90,63 @@ int contacts_person_create(libdbo_connection_t* connection, const char* name,
         return -1;
     }
 
-    if (libdbo_connection_connect(connection)) {
-        fprintf(stderr, "unable to connect to database!\n");
+    /*
+     * First connect to the database.
+     */
+    if ((error = libdbo_connection_connect(connection))) {
+        /*
+         * There was an error connection to the database, report what it was and
+         * return a failure.
+         */
+        fprintf(stderr, "unable to connect to database: %s\n", libdbo_errstr(error));
         return -5;
     }
 
+    /*
+     * This will create a person object by:
+     * - Allocate a new person object with <object>_new(), this is not added to
+     *   the database until we call the <object>_create() function.
+     * - Set all the fields with <object>_set_<field>() functions.
+     * - Create the object in the database with <object>_create().
+     */
     if (!(person = person_new(connection))
-        || person_set_name(person, name)
-        || person_set_birth_year(person, atoi(birth_year))
-        || person_set_address(person, address)
-        || person_set_phone(person, phone)
-        || person_set_email(person, email)
-        || person_create(person))
+        || (error = person_set_name(person, name))
+        || (error = person_set_birth_year(person, atoi(birth_year)))
+        || (error = person_set_address(person, address))
+        || (error = person_set_phone(person, phone))
+        || (error = person_set_email(person, email))
+        || (error = person_create(person)))
     {
+        /*
+         * One of the functions above failed so we do some cleanup, report
+         * the error and return a failure.
+         */
         person_free(person);
-        fprintf(stderr, "unable to create person!\n");
+        fprintf(stderr, "unable to create person: %s\n",
+            (person ? libdbo_errstr(error) : "allocation error"));
         return -6;
     }
-    person_free(person);
 
-    printf("Person created successfully.\n");
+    /*
+     * The object was created successfully.
+     */
+    printf("Person \"%s\" was created successfully.\n", person_name(person));
+    person_free(person);
 
     return 0;
 }
 
+/*
+ * This function puts a person into a folder, both the person and the folder
+ * must exists.
+ */
 int contacts_person_put(libdbo_connection_t* connection, const char* name,
     const char* folder)
 {
     person_t* person;
     folder_t* folder_object;
     folder_person_t* folder_person;
+    int error;
 
     if (!connection) {
         return -1;
@@ -120,36 +158,70 @@ int contacts_person_put(libdbo_connection_t* connection, const char* name,
         return -1;
     }
 
-    if (libdbo_connection_connect(connection)) {
-        fprintf(stderr, "unable to connect to database!\n");
+    /*
+     * First connect to the database.
+     */
+    if ((error = libdbo_connection_connect(connection))) {
+        /*
+         * There was an error connection to the database, report what it was and
+         * return a failure.
+         */
+        fprintf(stderr, "unable to connect to database: %s\n", libdbo_errstr(error));
         return -5;
     }
 
+    /*
+     * Get the person object by calling a specific <object>_new_get_by_<field>
+     * function, these functions will exist if the database type is an ID or
+     * an unique field.
+     */
     if (!(person = person_new_get_by_name(connection, name))) {
-        fprintf(stderr, "unable to find person!\n");
+        fprintf(stderr, "unable to find person \"%s\": %s\n", name, libdbo_errstr(error));
         return -6;
     }
 
+    /*
+     * Get the folder object by calling a specific <object>_new_get_by_<field>
+     * function, these functions will exist if the database type is an ID or
+     * an unique field.
+     */
     if (!(folder_object = folder_new_get_by_name(connection, folder))) {
         person_free(person);
-        fprintf(stderr, "unable to find folder!\n");
+        fprintf(stderr, "unable to find folder \"%s\": %s\n", folder, libdbo_errstr(error));
         return -6;
     }
 
+    /*
+     * This will create a folder_person object, that connects a person to a
+     * folder, by:
+     * - Allocate a new folder_person object with <object>_new(), this is not
+     *   added to the database until we call the <object>_create() function.
+     * - Set the person ID and folder ID with <object>_set_<field>() functions.
+     * - Create the object in the database with <object>_create().
+     */
     if (!(folder_person = folder_person_new(connection))
-        || folder_person_set_person_id(folder_person, person_id(person))
-        || folder_person_set_folder_id(folder_person, folder_id(folder_object))
-        || folder_person_create(folder_person))
+        || (error = folder_person_set_person_id(folder_person, person_id(person)))
+        || (error = folder_person_set_folder_id(folder_person, folder_id(folder_object)))
+        || (error = folder_person_create(folder_person)))
     {
+        /*
+         * One of the functions above failed so we do some cleanup, report
+         * the error and return a failure.
+         */
         folder_free(folder_object);
         person_free(person);
-        fprintf(stderr, "unable to put person in folder!\n");
+        fprintf(stderr, "unable to put person in folder: %s\n",
+            (folder_person ? libdbo_errstr(error) : "allocation error"));
         return -6;
     }
+
+    /*
+     * The object was created successfully.
+     */
+    printf("Person \"%s\" has been put into folder \"%s\" successfully.\n",
+        person_name(person), folder_name(folder_object));
     folder_free(folder_object);
     person_free(person);
-
-    printf("Person has been put into folder successfully.\n");
 
     return 0;
 }
@@ -157,13 +229,21 @@ int contacts_person_put(libdbo_connection_t* connection, const char* name,
 int contacts_person_list(libdbo_connection_t* connection) {
     person_list_t* person_list;
     const person_t* person;
+    int error;
 
     if (!connection) {
         return -1;
     }
 
-    if (libdbo_connection_connect(connection)) {
-        fprintf(stderr, "unable to connect to database!\n");
+    /*
+     * First connect to the database.
+     */
+    if ((error = libdbo_connection_connect(connection))) {
+        /*
+         * There was an error connection to the database, report what it was and
+         * return a failure.
+         */
+        fprintf(stderr, "unable to connect to database: %s\n", libdbo_errstr(error));
         return -5;
     }
 
@@ -199,6 +279,7 @@ int contacts_person_update(libdbo_connection_t* connection, const char* name,
     const char* field, const char* new_value)
 {
     person_t* person;
+    int error;
 
     if (!connection) {
         return -1;
@@ -213,46 +294,58 @@ int contacts_person_update(libdbo_connection_t* connection, const char* name,
         return -1;
     }
 
-    if (libdbo_connection_connect(connection)) {
-        fprintf(stderr, "unable to connect to database!\n");
+    /*
+     * First connect to the database.
+     */
+    if ((error = libdbo_connection_connect(connection))) {
+        /*
+         * There was an error connection to the database, report what it was and
+         * return a failure.
+         */
+        fprintf(stderr, "unable to connect to database: %s\n", libdbo_errstr(error));
         return -5;
     }
 
+    /*
+     * Get the person object by calling a specific <object>_new_get_by_<field>
+     * function, these functions will exist if the database type is an ID or
+     * an unique field.
+     */
     if (!(person = person_new_get_by_name(connection, name))) {
         fprintf(stderr, "unable to find person!\n");
         return -6;
     }
 
     if (!strcmp(field, "name")) {
-        if (person_set_name(person, new_value)) {
+        if ((error = person_set_name(person, new_value))) {
             person_free(person);
             fprintf(stderr, "unable to set field!\n");
             return -6;
         }
     }
     else if (!strcmp(field, "birth year")) {
-        if (person_set_birth_year(person, atoi(new_value))) {
+        if ((error = person_set_birth_year(person, atoi(new_value)))) {
             person_free(person);
             fprintf(stderr, "unable to set field!\n");
             return -6;
         }
     }
     else if (!strcmp(field, "address")) {
-        if (person_set_address(person, new_value)) {
+        if ((error = person_set_address(person, new_value))) {
             person_free(person);
             fprintf(stderr, "unable to set field!\n");
             return -6;
         }
     }
     else if (!strcmp(field, "phone")) {
-        if (person_set_phone(person, new_value)) {
+        if ((error = person_set_phone(person, new_value))) {
             person_free(person);
             fprintf(stderr, "unable to set field!\n");
             return -6;
         }
     }
     else if (!strcmp(field, "email")) {
-        if (person_set_email(person, new_value)) {
+        if ((error = person_set_email(person, new_value))) {
             person_free(person);
             fprintf(stderr, "unable to set field!\n");
             return -6;
@@ -264,7 +357,7 @@ int contacts_person_update(libdbo_connection_t* connection, const char* name,
         return -6;
     }
 
-    if (person_update(person)) {
+    if ((error = person_update(person))) {
         person_free(person);
         fprintf(stderr, "unable to update person!\n");
         return -6;
@@ -280,6 +373,7 @@ int contacts_person_delete(libdbo_connection_t* connection, const char* name) {
     person_t* person;
     folder_person_list_t* folder_person_list;
     folder_person_t* folder_person;
+    int error;
 
     if (!connection) {
         return -1;
@@ -288,11 +382,23 @@ int contacts_person_delete(libdbo_connection_t* connection, const char* name) {
         return -1;
     }
 
-    if (libdbo_connection_connect(connection)) {
-        fprintf(stderr, "unable to connect to database!\n");
+    /*
+     * First connect to the database.
+     */
+    if ((error = libdbo_connection_connect(connection))) {
+        /*
+         * There was an error connection to the database, report what it was and
+         * return a failure.
+         */
+        fprintf(stderr, "unable to connect to database: %s\n", libdbo_errstr(error));
         return -5;
     }
 
+    /*
+     * Get the person object by calling a specific <object>_new_get_by_<field>
+     * function, these functions will exist if the database type is an ID or
+     * an unique field.
+     */
     if (!(person = person_new_get_by_name(connection, name))) {
         fprintf(stderr, "unable to find person!\n");
         return -6;
@@ -308,7 +414,7 @@ int contacts_person_delete(libdbo_connection_t* connection, const char* name) {
         folder_person;
         folder_person_free(folder_person), folder_person = folder_person_list_get_next(folder_person_list))
     {
-        if (folder_person_delete(folder_person)) {
+        if ((error = folder_person_delete(folder_person))) {
             folder_person_free(folder_person);
             folder_person_list_free(folder_person_list);
             person_free(person);
@@ -318,7 +424,7 @@ int contacts_person_delete(libdbo_connection_t* connection, const char* name) {
     }
     folder_person_list_free(folder_person_list);
 
-    if (person_delete(person)) {
+    if ((error = person_delete(person))) {
         person_free(person);
         fprintf(stderr, "unable to delete person!\n");
         return -6;
@@ -332,6 +438,7 @@ int contacts_person_delete(libdbo_connection_t* connection, const char* name) {
 
 int contacts_folder_create(libdbo_connection_t* connection, const char* name) {
     folder_t* folder;
+    int error;
 
     if (!connection) {
         return -1;
@@ -340,15 +447,26 @@ int contacts_folder_create(libdbo_connection_t* connection, const char* name) {
         return -1;
     }
 
-    if (libdbo_connection_connect(connection)) {
-        fprintf(stderr, "unable to connect to database!\n");
+    /*
+     * First connect to the database.
+     */
+    if ((error = libdbo_connection_connect(connection))) {
+        /*
+         * There was an error connection to the database, report what it was and
+         * return a failure.
+         */
+        fprintf(stderr, "unable to connect to database: %s\n", libdbo_errstr(error));
         return -5;
     }
 
     if (!(folder = folder_new(connection))
-        || folder_set_name(folder, name)
-        || folder_create(folder))
+        || (error = folder_set_name(folder, name))
+        || (error = folder_create(folder)))
     {
+        /*
+         * One of the functions above failed so we do some cleanup, report
+         * the error and return a failure.
+         */
         folder_free(folder);
         fprintf(stderr, "unable to create folder!\n");
         return -6;
@@ -361,13 +479,21 @@ int contacts_folder_create(libdbo_connection_t* connection, const char* name) {
 }
 
 int contacts_folder_list(libdbo_connection_t* connection, const char* name) {
+    int error;
 
     if (!connection) {
         return -1;
     }
 
-    if (libdbo_connection_connect(connection)) {
-        fprintf(stderr, "unable to connect to database!\n");
+    /*
+     * First connect to the database.
+     */
+    if ((error = libdbo_connection_connect(connection))) {
+        /*
+         * There was an error connection to the database, report what it was and
+         * return a failure.
+         */
+        fprintf(stderr, "unable to connect to database: %s\n", libdbo_errstr(error));
         return -5;
     }
 
@@ -406,9 +532,13 @@ int contacts_folder_list(libdbo_connection_t* connection, const char* name) {
         }
 
         if (!(folder_person_list = folder_person_list_new(connection))
-            || folder_person_list_associated_fetch(folder_person_list)
-            || folder_person_list_get_by_folder_id(folder_person_list, folder_id(folder)))
+            || (error = folder_person_list_associated_fetch(folder_person_list))
+            || (error = folder_person_list_get_by_folder_id(folder_person_list, folder_id(folder))))
         {
+            /*
+             * One of the functions above failed so we do some cleanup, report
+             * the error and return a failure.
+             */
             folder_free(folder);
             fprintf(stderr, "unable to get persons in folder!\n");
             return -6;
@@ -438,6 +568,7 @@ int contacts_folder_delete(libdbo_connection_t* connection, const char* name) {
     folder_t* folder;
     folder_person_list_t* folder_person_list;
     folder_person_t* folder_person;
+    int error;
 
     if (!connection) {
         return -1;
@@ -446,11 +577,23 @@ int contacts_folder_delete(libdbo_connection_t* connection, const char* name) {
         return -1;
     }
 
-    if (libdbo_connection_connect(connection)) {
-        fprintf(stderr, "unable to connect to database!\n");
+    /*
+     * First connect to the database.
+     */
+    if ((error = libdbo_connection_connect(connection))) {
+        /*
+         * There was an error connection to the database, report what it was and
+         * return a failure.
+         */
+        fprintf(stderr, "unable to connect to database: %s\n", libdbo_errstr(error));
         return -5;
     }
 
+    /*
+     * Get the folder object by calling a specific <object>_new_get_by_<field>
+     * function, these functions will exist if the database type is an ID or
+     * an unique field.
+     */
     if (!(folder = folder_new_get_by_name(connection, name))) {
         fprintf(stderr, "unable to find folder!\n");
         return -6;
@@ -466,7 +609,7 @@ int contacts_folder_delete(libdbo_connection_t* connection, const char* name) {
         folder_person;
         folder_person_free(folder_person), folder_person = folder_person_list_get_next(folder_person_list))
     {
-        if (folder_person_delete(folder_person)) {
+        if ((error = folder_person_delete(folder_person))) {
             folder_person_free(folder_person);
             folder_person_list_free(folder_person_list);
             folder_free(folder);
@@ -476,7 +619,7 @@ int contacts_folder_delete(libdbo_connection_t* connection, const char* name) {
     }
     folder_person_list_free(folder_person_list);
 
-    if (folder_delete(folder)) {
+    if ((error = folder_delete(folder))) {
         folder_free(folder);
         fprintf(stderr, "unable to delete folder!\n");
         return -6;
@@ -489,7 +632,7 @@ int contacts_folder_delete(libdbo_connection_t* connection, const char* name) {
 }
 
 int main(int argc, char* argv[]) {
-    int opt, ret;
+    int opt, ret, error;
     libdbo_configuration_list_t* configuration_list = NULL;
     libdbo_configuration_t* configuration = NULL;
     libdbo_connection_t* connection = NULL;
@@ -512,9 +655,9 @@ int main(int argc, char* argv[]) {
                 || (*value = 0) != 0
                 || !(*(value + 1))
                 || !(configuration = libdbo_configuration_new())
-                || libdbo_configuration_set_name(configuration, name)
-                || libdbo_configuration_set_value(configuration, value + 1)
-                || libdbo_configuration_list_add(configuration_list, configuration))
+                || (error = libdbo_configuration_set_name(configuration, name))
+                || (error = libdbo_configuration_set_value(configuration, value + 1))
+                || (error = libdbo_configuration_list_add(configuration_list, configuration)))
             {
                 free(name);
                 libdbo_configuration_free(configuration);
@@ -533,8 +676,8 @@ int main(int argc, char* argv[]) {
     }
 
     if (!(connection = libdbo_connection_new())
-        || libdbo_connection_set_configuration_list(connection, configuration_list)
-        || libdbo_connection_setup(connection))
+        || (error = libdbo_connection_set_configuration_list(connection, configuration_list))
+        || (error = libdbo_connection_setup(connection)))
     {
         libdbo_connection_free(connection);
         libdbo_configuration_list_free(configuration_list);
